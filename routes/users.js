900f48652e1796f914416e3cf4c77d0acde81cf8
Doan Cong Thanh <thanhdoan020999@gmail.com>
2020-12-27 23:44:32 +0700

    add teachers for class, set permission

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controller/users');
 const user = require('../models/users');
+const classes = require('../models/classes');
 
 
 /* GET users listing. */
@@ -9,15 +10,17 @@ router.get('/1', function (req, res, next) {
   res.send('I AM USER 1');
 });
 
+
 router.get('/', userController.getAllUser);
 
-router.get('/ajax-users', async (req, res, next) => {
-  const users = await user.find();
-  res.send(users);
-})
+router.get('/user-detail/:username', userController.getUserDetail);
+
+router.get('/ajax-users', userController.getInviteUser)
+
+router.get('/ajax-teachers', userController.getInviteTeacher)
 
 router.post('/ajax-users/add', userController.postInviteUser)
 
-router.get('/user-detail/:username', userController.getUserDetail);
+router.post('/ajax-teachers/add', userController.postInviteTeacher)
 
 module.exports = router;
