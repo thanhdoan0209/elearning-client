a4405bc7546004b75ff3b6d3e374d8ada6038c16
Doan Cong Thanh <thanhdoan020999@gmail.com>
2020-12-27 15:59:59 +0700

    add user for class, UI banner

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controller/users');
+const user = require('../models/users');
+
 
 /* GET users listing. */
 router.get('/1', function (req, res, next) {
@@ -8,6 +10,13 @@ router.get('/1', function (req, res, next) {
 });
 
 router.get('/', userController.getAllUser);
+
+router.get('/ajax-users', async (req, res, next) => {
+  const users = await user.find();
+  res.send(users);
+})
+
+router.post('/ajax-users/add', userController.postInviteUser)
 
 router.get('/user-detail/:username', userController.getUserDetail);
 
